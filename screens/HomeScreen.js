1ed8a478cfc0d3bf497a7d2c4fbc64afe99679c3
ChravisDeYoung <dejong_travis@yahoo.ca>
2022-04-11 23:10:50 -0400

    mock matches in history

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,57 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 
 import UserTab from "../components/UserTab";
+import MatchSummary from "../components/MatchSummary";
 
 const HomeScreen = (props) => {
+  const matches = [
+    {
+      date: "February 3",
+      location: "Dorchester, ON",
+      status: "Loss",
+      opponent: {
+        name: "Jane Doe",
+        image: "https://reactnative.dev/img/tiny_logo.png",
+      },
+      home: [6, 5, 2],
+      away: [3, 7, 6],
+    },
+    {
+      date: "March 23",
+      location: "London, ON",
+      status: "Win",
+      opponent: {
+        name: "Saul Goodman",
+        image: "https://reactnative.dev/img/tiny_logo.png",
+      },
+      home: [6, 4, 6],
+      away: [4, 6, 2],
+    },
+    {
+      date: "February 3",
+      location: "Dorchester, ON",
+      status: "Loss",
+      opponent: {
+        name: "Jane Doe",
+        image: "https://reactnative.dev/img/tiny_logo.png",
+      },
+      home: [6, 5, 2],
+      away: [3, 7, 6],
+    },
+    {
+      date: "March 23",
+      location: "London, ON",
+      status: "Win",
+      opponent: {
+        name: "Saul Goodman",
+        image: "https://reactnative.dev/img/tiny_logo.png",
+      },
+      home: [6, 4, 6],
+      away: [4, 6, 2],
+    },
+  ];
+
   return (
     <View>
       <UserTab
@@ -11,9 +59,26 @@ const HomeScreen = (props) => {
         username="John Doe"
         navigation={props.navigation}
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("New Match")}>
-        <Text>New Match</Text>
+      <Text style={{ fontSize: 20, textAlign: "center", padding: 10 }}>
+        Match History
+      </Text>
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("New Match")}
+        style={{
+          backgroundColor: "#c4c4c4",
+          width: 100,
+          alignSelf: "center",
+          padding: 5,
+          marginBottom: 15,
+        }}
+      >
+        <Text style={{ fontSize: 15, textAlign: "center" }}>New Match</Text>
       </TouchableOpacity>
+      <ScrollView style={{ height: "65%" }}>
+        {matches.map((match, index) => (
+          <MatchSummary match={match} key={index} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
