0449ba193e920a9b24c087ff16d2f5b1d66bfb47
ChravisDeYoung <dejong_travis@yahoo.ca>
2022-04-11 21:19:57 -0400

    profile tab redirects to the edit profile

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 
+import UserTab from "../components/UserTab";
+
 const HomeScreen = (props) => {
   return (
     <View>
-      <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}>
-        <Text>Edit Profile</Text>
-      </TouchableOpacity>
+      <UserTab
+        imageUri="https://reactnative.dev/img/tiny_logo.png"
+        username="John Doe"
+        navigation={props.navigation}
+      />
       <TouchableOpacity onPress={() => props.navigation.navigate("New Match")}>
         <Text>New Match</Text>
       </TouchableOpacity>
