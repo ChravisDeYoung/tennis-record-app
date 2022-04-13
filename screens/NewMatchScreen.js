86042840c0a928065672ed9f1bc4f59445a8cb23
ChravisDeYoung <dejong_travis@yahoo.ca>
2022-04-13 13:04:36 -0400

    created basic layout

--- screens/NewMatchScreen.js
+++ screens/NewMatchScreen.js
@@ -1,10 +1,110 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
-const NewMatchScreen = () => {
+import UserTab from "../components/UserTab";
+
+const NewMatchScreen = (props) => {
   return (
     <View>
-      <Text>Add Match</Text>
+      <UserTab
+        imageUri="https://reactnative.dev/img/tiny_logo.png"
+        username="John Doe"
+        navigation={props.navigation}
+      />
+      <Text style={{ fontSize: 20, textAlign: "center", marginTop: 20 }}>
+        New Match
+      </Text>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text style={{ fontSize: 15, paddingBottom: 10, paddingTop: 20 }}>
+          Against
+        </Text>
+        <TextInput
+          placeholder="John Doe"
+          style={{ backgroundColor: "#c4c4c4", fontSize: 15, padding: 10 }}
+        />
+      </View>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text style={{ fontSize: 15, paddingBottom: 10, paddingTop: 20 }}>
+          Date
+        </Text>
+        <TextInput
+          placeholder="April 20, 2021"
+          style={{ backgroundColor: "#c4c4c4", fontSize: 15, padding: 10 }}
+        />
+      </View>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text style={{ fontSize: 15, paddingBottom: 10, paddingTop: 20 }}>
+          Your Score
+        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+        </View>
+      </View>
+      <View style={{ paddingHorizontal: 20 }}>
+        <Text style={{ fontSize: 15, paddingBottom: 10, paddingTop: 20 }}>
+          Their Score
+        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+          <TextInput
+            style={{
+              backgroundColor: "#c4c4c4",
+              padding: 10,
+              marginHorizontal: 10,
+            }}
+            placeholder="0"
+          />
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#c4c4c4",
+          alignSelf: "center",
+          paddingVertical: 10,
+          paddingHorizontal: 20,
+          margin: 15,
+        }}
+      >
+        <Text style={{ fontSize: 15 }}>Finish</Text>
+      </TouchableOpacity>
     </View>
   );
 };
