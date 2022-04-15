f3d7cf907402eeadaad9a07edc764bb8559fa8f8
ChravisDeYoung <dejong_travis@yahoo.ca>
2022-04-15 11:22:57 -0400

    fixed auth screen

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import TennisBall from "../components/TennisBall";
 
@@ -47,29 +53,13 @@ const AuthScreen = (props) => {
   };
 
   return (
-    <View style={{ height: "100%", overflow: "hidden" }}>
+    <View style={styles.mainScreen}>
       <TennisBall rotation="30deg" top={-20} left={-20} />
       <TennisBall rotation="-50deg" top={150} left={150} />
       <TennisBall rotation="80deg" top={300} left={20} />
       <TennisBall rotation="120deg" top={500} left={120} />
-      <View
-        style={{
-          backgroundColor: "#1985ff",
-          width: "80%",
-          alignSelf: "center",
-          padding: 15,
-          marginTop: "30%",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            textAlign: "center",
-            marginBottom: 10,
-            color: "white",
-          }}
-        >
+      <View style={styles.authContainer}>
+        <Text style={styles.title}>
           {showRegister ? "Sign up" : "Welcome back"}
         </Text>
         <TextInput
@@ -79,15 +69,13 @@ const AuthScreen = (props) => {
           autoCompleteType="email"
           keyboardType="email-address"
           placeholder="email"
-          style={{
-            backgroundColor: "white",
-            margin: 10,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-            fontSize: 15,
-            borderColor: errorText !== "" ? "red" : "white",
-            borderWidth: errorText !== "" ? 1 : 0,
-          }}
+          style={[
+            styles.input,
+            {
+              borderColor: errorText !== "" ? "red" : "white",
+              borderWidth: errorText !== "" ? 1 : 0,
+            },
+          ]}
         />
         <TextInput
           onChangeText={(value) => setPassword(value)}
@@ -97,38 +85,20 @@ const AuthScreen = (props) => {
           autoCompleteType="password"
           keyboardType="visible-password"
           placeholder="password"
-          style={{
-            backgroundColor: "white",
-            margin: 10,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-            fontSize: 15,
-            borderColor: errorText !== "" ? "red" : "white",
-            borderWidth: errorText !== "" ? 1 : 0,
-          }}
+          style={[
+            styles.input,
+            {
+              borderColor: errorText !== "" ? "red" : "white",
+              borderWidth: errorText !== "" ? 1 : 0,
+            },
+          ]}
         />
-        {errorText !== "" && (
-          <Text style={{ color: "red", textAlign: "center" }}>{errorText}</Text>
-        )}
+        {errorText !== "" && <Text style={styles.error}>{errorText}</Text>}
         <TouchableOpacity
           onPress={showRegister ? registerWithFirebase : loginWithFirebase}
-          style={{
-            backgroundColor: "white",
-            padding: 10,
-            textAlign: "center",
-            width: "30%",
-            alignSelf: "center",
-            margin: 10,
-          }}
+          style={styles.button}
         >
-          <Text
-            style={{
-              fontSize: 15,
-              textAlign: "center",
-              color: "#1985ff",
-              fontWeight: "bold",
-            }}
-          >
+          <Text style={styles.buttonText}>
             {showRegister ? "Register" : "Log in"}
           </Text>
         </TouchableOpacity>
@@ -148,5 +118,51 @@ const AuthScreen = (props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  authContainer: {
+    backgroundColor: "#1985ff",
+    width: "80%",
+    alignSelf: "center",
+    padding: 15,
+    marginTop: "30%",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+    color: "white",
+  },
+  mainScreen: { height: "100%", overflow: "hidden" },
+  input: {
+    backgroundColor: "white",
+    margin: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    fontSize: 15,
+  },
+  error: {
+    color: "red",
+    textAlign: "center",
+    backgroundColor: "white",
+    padding: 5,
+    marginHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    textAlign: "center",
+    alignSelf: "center",
+    margin: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    textAlign: "center",
+    color: "#1985ff",
+    fontWeight: "bold",
+  },
+});
 
 export default AuthScreen;
